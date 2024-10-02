8e8c0ec1ca714cee61c4a2a30c8fd989c9b1941e
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2024-10-02 13:52:26 +0400

    add new solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor(number, average) {
+        number; //загадочное число
+        average = Math.floor((this.max + this.min) / 2);
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        //определить переменные мин и макс значений
     }
 
     guess() {
-
+        this.guessNum = Math.round((this.max + this.min) / 2);
+        return this.guessNum;
+        //сравнить загаданное число с серединой
     }
 
     lower() {
-
+        this.max = this.guessNum;
+        //функция, которая идет в левую половину, двигаем максимум на значение середины
     }
 
     greater() {
-
+        this.min = this.guessNum;
+        //функция, которая идет в правую половину, двигаем минимум на значение середины
     }
 }
 
